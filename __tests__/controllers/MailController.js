665a56c42c59a5149870098ab0a90b080606e725
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-02-12 22:48:49 +0200

    feat(notification): import mock data without destructing

--- __tests__/controllers/MailController.js
+++ __tests__/controllers/MailController.js
@@ -1,10 +1,10 @@
-import { mailUser } from '../mocks/db.json';
+import db from '../mocks/db.json';
 import { MailController } from '../../controllers';
 
 describe('sendgrid', () => {
   test('should send the confirmation email', async () => {
     expect.assertions(3);
-    const res = await MailController.sendConfirmationEmail(mailUser);
+    const res = await MailController.sendConfirmationEmail(db.mailUser);
     expect(res).toBeDefined();
     expect(res.length).toBeGreaterThan(0);
     expect(res[0].statusCode).toBe(202);

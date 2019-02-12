8989bac491c406632e406a69dbdd49e4ed016b08
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-02-12 11:35:51 +0200

    feat(notification): correct hound-ci violations

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,13 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coveragePathIgnorePatterns: ['node_modules', 'index.js', 'app.js', 'database', '__tests__'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'middlewares',
+    'index.js',
+    'app.js',
+    'database',
+    '__tests__'
+  ],
   coverageDirectory: 'coverage'
 };

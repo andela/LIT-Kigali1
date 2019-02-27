1ccab4c20f210eb00ac6d96ee06169fb21435d18
Nkaka Manzi Fab <fabjeezy@gmail.com>
2019-02-27 17:08:31 +0200

    #163519142 Social login through Facebook, Twitter and Google (#19)
    * chore: Fix test to meet threshold
    
    * chore: Fix hound CI violations
    
    * chore: Fix hound CI comments
    
    * chore: Fix hound CI comments
    
    * chrore: Fix eslinnt rules

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -20,44 +20,32 @@ class ProfileController {
       let message;
       let errorMessage;
       if (email && username) {
-        try {
-          const emailOwner = await User.findOne({
-            where: { email }
-          });
-          const usernameOwner = await User.findOne({
-            where: { username }
-          });
-          if (emailOwner && usernameOwner) {
-            message = 'email and username are';
-          } else if (emailOwner) {
-            message = 'email is';
-          } else if (usernameOwner) {
-            message = 'username is';
-          }
-        } catch (error) {
-          errorMessage = error.message;
+        const emailOwner = await User.findOne({
+          where: { email }
+        });
+        const usernameOwner = await User.findOne({
+          where: { username }
+        });
+        if (emailOwner && usernameOwner) {
+          message = 'email and username are';
+        } else if (emailOwner) {
+          message = 'email is';
+        } else if (usernameOwner) {
+          message = 'username is';
         }
       } else if (email) {
-        try {
-          const emailOwner = await User.findOne({
-            where: { email }
-          });
-          if (emailOwner) {
-            message = 'email is';
-          }
-        } catch (error) {
-          errorMessage = error.message;
+        const emailOwner = await User.findOne({
+          where: { email }
+        });
+        if (emailOwner) {
+          message = 'email is';
         }
       } else if (username) {
-        try {
-          const usernameOwner = await User.findOne({
-            where: { username }
-          });
-          if (usernameOwner) {
-            message = 'username is';
-          }
-        } catch (error) {
-          errorMessage = error.message;
+        const usernameOwner = await User.findOne({
+          where: { username }
+        });
+        if (usernameOwner) {
+          message = 'username is';
         }
       }
       if (message) {
@@ -75,37 +63,29 @@ class ProfileController {
         });
       }
     }
-    try {
-      const profile = await User.findOne({
-        attributes: { exclude: ['password', 'status', 'userType', 'createdAt'] },
-        where: { id }
-      });
-      profile.update({
-        updateAt: new Date(),
-        ...user,
-        confirmationCode: uuid.v4(),
-        confirmed: 'pending'
-      });
-      let message;
-      if (user.email) {
-        await sendEmailConfirmationLink({ ...profile.get() });
-        message = 'Your email has changed. Please check your email for confirmation';
-      } else {
-        message = 'The information was updated successful';
-      }
-      const { confirmationCode, ...userData } = profile.get();
-      return res.status(200).send({
-        statu: 200,
-        message,
-        user: userData
-      });
-    } catch (error) {
-      return res.status(520).send({
-        errors: {
-          body: [error.message]
-        }
-      });
+    const profile = await User.findOne({
+      attributes: { exclude: ['password', 'status', 'userType', 'createdAt'] },
+      where: { id }
+    });
+    profile.update({
+      updateAt: new Date(),
+      ...user,
+      confirmationCode: uuid.v4(),
+      confirmed: 'pending'
+    });
+    let message;
+    if (user.email) {
+      await sendEmailConfirmationLink({ ...profile.get() });
+      message = 'Your email has changed. Please check your email for confirmation';
+    } else {
+      message = 'The information was updated successful';
     }
+    const { confirmationCode, ...userData } = profile.get();
+    return res.status(200).send({
+      statu: 200,
+      message,
+      user: userData
+    });
   }
 }
 

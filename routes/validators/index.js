428afb01db4f442f1eeb201fd14c2beccf262295
Nziranziza <nziranzizadaniel@gmail.com>
2019-02-28 16:37:55 +0200

    feat(rating) 5 stars rating

--- routes/validators/index.js
+++ routes/validators/index.js
@@ -1,11 +1,11 @@
 import authValidator from './auth';
 import articleValidator from './article';
 import profileValidator from './user';
+
 import commentValidator from './comment';
 import ratingValidator from './rating';
 
 
 export {
- authValidator, articleValidator, profileValidator, commentValidator 
+  authValidator, articleValidator, profileValidator, commentValidator, ratingValidator
 };
-

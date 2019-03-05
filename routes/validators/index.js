3304f0525e0463816123e49356b20c9bb65af4a2
Nziranziza <nziranzizadaniel@gmail.com>
2019-03-05 11:12:54 +0200

    feat(rating) 5 stars rating - add rate controller - add delete rate controller - add test - add post route to add rate - add delete route to remove rate - add validator to validate the req.body [ Continues #163519145]

--- routes/validators/index.js
+++ routes/validators/index.js
@@ -2,6 +2,7 @@ import authValidator from './auth';
 import articleValidator from './article';
 import profileValidator from './user';
 import commentValidator from './comment';
+import ratingValidator from './rating';
 
 
 export {

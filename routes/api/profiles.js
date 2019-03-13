768f9d088123c3a65a7bdede89ff9c4e059ddd1f
nkfab <fabjeezy@gmail.com>
2019-03-13 11:34:08 +0200

    fix(pages): add pagination list of profiles

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { celebrate } from 'celebrate';
 import { FollowController, ProfileController } from '../../controllers';
 import { verifyJwt } from '../../middlewares';
+import { profilesValidator } from '../validators';
 
 dotenv.config();
 const router = express.Router();
@@ -9,7 +11,12 @@ router.post('/:username/follow', verifyJwt(), FollowController.follow);
 
 router.delete('/:username/follow', verifyJwt(), FollowController.unfollow);
 
-router.get('/', verifyJwt({ tokenRequired: false }), ProfileController.getProfiles);
+router.get(
+  '/',
+  celebrate({ query: profilesValidator.getProfiles }),
+  verifyJwt({ tokenRequired: false }),
+  ProfileController.getProfiles
+);
 
 router.get('/:username', ProfileController.getProfile);
 

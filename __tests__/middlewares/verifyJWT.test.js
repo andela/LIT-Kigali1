1f8a038672566c0f31a632e34b7a428ff393caf2
Nziranziza <nziranzizadaniel@gmail.com>
2019-03-14 18:15:48 +0200

    feat(acess) role based access control - add more tests

--- __tests__/middlewares/verifyJWT.test.js
+++ __tests__/middlewares/verifyJWT.test.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import request from 'supertest';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { verifyJwt } from '../../middlewares';
 import app from '../../app';
-import { User } from '../../database/models';
+import { User, Token } from '../../database/models';
 import { signupUser, signupUser2 } from '../mocks/db.json';
 import { urlPrefix } from '../mocks/variables.json';
 
+const { JWT_SECRET } = process.env;
 const appTest = express();
 const router = express.Router();
 
@@ -14,6 +16,7 @@ const router = express.Router();
 router.get('/testJwtWithUser', verifyJwt({ access: ['user'] }));
 router.get('/testJwtwithSuperAdmin', verifyJwt({ access: ['super-admin'] }));
 router.get('/testJwtwithAdmin', verifyJwt({ access: ['admin'] }));
+router.get('/testWithConfirmEmail', verifyJwt({ confirmEmail: true }));
 
 appTest.use(router);
 let admin;
@@ -52,6 +55,14 @@ describe('verifyJWT', () => {
       });
     user2 = res2.body.user;
   });
+  afterAll(async () => {
+    await User.destroy({
+      where: { id: admin.id }
+    });
+    await User.destroy({
+      where: { id: user2.id }
+    });
+  });
 
   test('it should fail without user token for user only router', async () => {
     const res = await request(appTest)
@@ -78,5 +89,44 @@ describe('verifyJWT', () => {
     expect(res.status).toBe(401);
     expect(res.body.status).toBe(401);
     expect(res.body.message).toBe('Not authorized');
+  });
+
+  test('it should fail when email is not confirmed', async () => {
+    const user = await User.findOne({
+      where: { id: user2.id }
+    });
+    user.update({ confirmed: 'pending' });
+    const { body } = await request(app)
+      .post(`${urlPrefix}/users/login`)
+      .send({
+        user: {
+          username: signupUser2.email,
+          password: signupUser2.password
+        }
+      });
+    const res = await request(appTest)
+      .get('/testWithConfirmEmail')
+      .set('authorization', body.user.token);
+    expect(res.status).toBe(401);
+    expect(res.body.status).toBe(401);
+    expect(res.body.message).toBe('Please confirm your email');
+  });
+  test('it should return unexpected', async () => {
+    const token = jwt.sign({ id: 24374, userType: 'user.userType' }, JWT_SECRET);
+    const tokenRecord = await Token.findOne({ where: { userId: user2.id } });
+    tokenRecord.update({ token });
+    const res = await request(appTest)
+      .get('/testJwtWithAdmin')
+      .set('authorization', token);
+    expect(res.status).toBe(520);
+    expect(res.body.errors).toBeDefined();
+  });
+
+  test('it should return unexpected', async () => {
+    const res = await request(appTest)
+      .get('/testJwtWithAdmin')
+      .set('authorization', 'edhfh');
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('jwt malformed');
   });
 });

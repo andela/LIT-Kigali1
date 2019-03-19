ad70b672c6aaf2f06fb49289d2beb7a61b3b9a30
nkfab <fabjeezy@gmail.com>
2019-03-19 14:24:04 +0200

    fix(tests): fix build failed

--- __tests__/routes/article.test.js
+++ __tests__/routes/article.test.js
@@ -3,7 +3,15 @@ import { Op } from 'sequelize';
 import bcrypt from 'bcrypt';
 import app from '../../app';
 import { urlPrefix } from '../mocks/variables.json';
-import { User, Article, Favorite, Report, Notification, Reader} from '../../database/models';
+import {
+  User,
+  Article,
+  Favorite,
+  Report,
+  Notification,
+  Reader,
+  Bookmark
+} from '../../database/models';
 import { createArticle, signupUser, createArticleTwo } from '../mocks/db.json';
 
 let loginUser1;
